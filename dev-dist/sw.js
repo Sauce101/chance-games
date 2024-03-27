ce0460bc67c8d2e67c05fcedf4fd8a73a1d8d162
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2024-03-27 09:14:39 -0700

    portrait height auto

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c676b6d3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jv0ske1aqmg"
+    "revision": "0.i3apq8t74a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
